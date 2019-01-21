c6889a01798cd6e9af0e5137542c1696c7c5a08a
kawazu <kawazugekogeko@gmail.com>
2019-01-21 22:42:01 +0900

    Avatarの実装を切り替えながらURLを取得する
    リファクタリング

--- avatar.go
+++ avatar.go
@@ -14,40 +14,53 @@ type Avatar interface {
 	// GetAvatarURLは指定されたクライアントのアバターのURLを返す
 	// 問題が生じた場合はエラーを返す。特に、URLを取得できなかった場合は
 	// ErrNoAvatarURLを返す
-	GetAvatarURL(c *client) (string, error)
+	GetAvatarURL(ChatUser) (string, error)
 }
 
 type GravatarAvatar struct{}
 
-var UseGravatar GravatarAvatar
+type TryAvatars []Avatar
 
-func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	if userid, ok := c.userData["userid"]; ok {
-		if useridStr, ok := userid.(string); ok {
-			return "//www/gracatar.com/avatar/" + useridStr, nil
+func (a TryAvatars) GetAvatarURL(u ChatUser) (string, error) {
+	for _, avatar := range a {
+		if url, err := avatar.GetAvatarURL(u); err == nil {
+			return url, nil
 		}
 	}
 	return "", ErrNoAvatarURL
+}
+
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	url := u.AvatarURL()
+	if url != "" {
+		return url, nil
+	}
+	return "", ErrNoAvatarURL
+}
+
+var UseGravatar GravatarAvatar
+
+func (_ GravatarAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	return "//www/gracatar.com/avatar/" + u.UniqueID(), nil
 }
 
 type FileSystemAvatar struct{}
 
 var UseFileSystemAvatar FileSystemAvatar
 
-func (_ FileSystemAvatar) GetAvatarURL(c *client) (string, error) {
-	if userid, ok := c.userData["userid"]; ok {
-		if useridStr, ok := userid.(string); ok {
-			if files, err := ioutil.ReadDir("avatars"); err == nil {
-				for _, file := range files {
-					if file.IsDir() {
-						continue
-					}
-					if match, _ := filepath.Match(useridStr+"*", file.Name()); match {
-						return "/avatars/" + file.Name(), nil
-					}
-				}
+func (_ FileSystemAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	if files, err := ioutil.ReadDir("avatars"); err == nil {
+		for _, file := range files {
+			if file.IsDir() {
+				continue
 			}
-			return "/avatars/" + useridStr + ".jpg", nil
+			if match, _ := filepath.Match(u.UniqueID()+"*", file.Name()); match {
+				return "/avatars/" + file.Name(), nil
+			}
 		}
 	}
 	return "", ErrNoAvatarURL
--- main.go
+++ main.go
@@ -17,7 +17,13 @@ import (
 	"github.com/stretchr/objx"
 )
 
-// temp1は一つのテンプレートをさす
+// 現在アクティブなAvatarの実装
+var avatars Avatar = TryAvatars{
+	UseFileSystemAvatar,
+	UseAuthAvatar,
+	UseGravatar}
+
+	// temp1は一つのテンプレートをさす
 type templateHandler struct {
 	once     sync.Once
 	filename string
@@ -51,7 +57,7 @@ func main() {
 		github.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/github"),
 		google.New(os.Getenv("CLIENT_FOR_CHAT"), os.Getenv("KEY_FOR_CHAT"), "http://localhost:8080/auth/callback/google"),
 	)
-	r := newRoom(UseFileSystemAvatar)
+	r := newRoom()
 	// if you want traceOff, you must comment this line out.
 	r.tracer = trace.New(os.Stdout)
 	// TODO:　localのbootstrapを適用させる
